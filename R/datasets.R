71256f0c67ee5036830253e763db163ffceb0a0a
Simon Tscholl <tscholl.simon@gmail.com>
2020-07-02 10:53:00 +0200

    added aspectre and aspect_map

--- R/datasets.R
+++ R/datasets.R
@@ -24,6 +24,33 @@
 #' @source \url{https://land.copernicus.eu/imagery-in-situ/eu-dem/eu-dem-v1-0-and-derived-products/eu-dem-v1.0}
 "aspect"
 
+#' Aspect raster of South Tyrol reclassified to values from 1 - 9
+#'
+#' Aspect raster for South Tyrol with a 20km buffer at 25m resolution. Calculated from dem
+#' using the aspect function in ArcGIS Pro. Method set to planar. Original values have been
+#' reclassified to values between 1 and 9. To see what the single values mean check out the
+#' table 'aspect_map' in the Rgadgets package.
+#'
+#' @format A raster file loaded using raster::raster()
+#' @source \url{https://land.copernicus.eu/imagery-in-situ/eu-dem/eu-dem-v1-0-and-derived-products/eu-dem-v1.0}
+"aspectre"
+
+#' Reclassify table for aspect values in degree
+#'
+#' A data.frame containing the range of aspect values and their corresponding exposition as a string ('nort', 'sout' ...)
+#'
+#' @format A data frame with 10 rows and 4 variables:
+#' \describe{
+#' \item{from}{start value of aspect in degree}
+#' \item{to}{end value of aspect in degree}
+#' \item{code}{new integer code that has been assigned to the aspect range}
+#' \item{aspect_str}{string representation of orientation, n = north, ne = north-east, e = east, se = south-east,
+#' s = south, sw = south-west, w = west, nw = north-west, f = flat}
+#' }
+#'
+#' @source \url{https://desktop.arcgis.com/de/arcmap/10.3/tools/spatial-analyst-toolbox/how-aspect-works.htm}
+"aspect_map"
+
 #' Border of South Tyrol
 #'
 #' Polygon delineating the border or South Tyrol (Italy)
