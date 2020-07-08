305eee759bff2c982f96d90aa812ad38c7cd9f3c
Simon Tscholl <tscholl.simon@gmail.com>
2020-07-08 09:07:16 +0200

    simplified and renamed rg_get_province to rg_province_get function

--- R/download_province.R
+++ R/download_province.R
@@ -5,9 +5,12 @@
 #' it will be possible to speed up the actual processing times and to make multiple requests at once
 #' @param dburl URL; URL of the Province Database. If left empty the original API will be used
 #' @param station_code string; Code of the station ("SCODE")
-#' @param sensor_code string; Abbreviation of the sensor of interest (e.g. "N" for Precipitation)
-#' @param datestart string; Starting time for the download in "Ymd" Format
-#' @param dateend string; End time for the download in "Ymd" Format
+#' @param sensor_code string; Abbreviation of the sensor of interest (e.g. "N" for Precipitation).
+#' For possible values check the Rgadgets::rg_province_info() function.
+#' @param datestart date; Starting date for the download
+#' @param dateend date; End date for the download
+#' @param format string, wide or long for a wide or long table as ar result
+#' @source \link{https://github.com/mattia6690/MonalisR/blob/master/R/Processing.R}
 #' @importFrom dplyr mutate select rename arrange
 #' @importFrom magrittr "%>%" extract
 #' @importFrom lubridate as_datetime
@@ -19,18 +22,18 @@
 #' @importFrom tidyselect everything
 #' @export
 
-rg_get_province <- function(dburl=NULL, station_code, sensor_code, datestart, dateend){
+rg_province_get <- function(dburl=NULL, station_code, sensor_code, datestart, dateend, format = 'wide'){
 
   if(is.null(dburl)) dburl<- "http://daten.buergernetz.bz.it/services/meteo/v1/timeseries"
-  datestart1 <- convertDate(datestart, db="Meteo")
-  dateend1   <- convertDate(dateend, db="Meteo")
 
-  dat<-expand.grid(station_code,sensor_code,stringsAsFactors = FALSE) %>%
+  dat <-
+    expand.grid(station_code, sensor_code, stringsAsFactors = FALSE) %>%
     as_tibble() %>%
     setNames(c("SCODE","Sensor")) %>%
-    mutate(Start=format(as.Date(datestart),format="%Y%m%d")) %>%
-    mutate(End=format(as.Date(dateend),format="%Y%m%d")) %>%
-    mutate(URL=pmap_chr(.,function(SCODE,Sensor,Start,End){
+
+    mutate(Start = format(datestart, format="%Y%m%d")) %>%
+    mutate(End = format(dateend, format="%Y%m%d")) %>%
+    mutate(URL = pmap_chr(., function(SCODE, Sensor, Start, End){
 
       dburl %>%
         paste0(.,"?station_code=",SCODE) %>%
@@ -40,23 +43,39 @@ rg_get_province <- function(dburl=NULL, station_code, sensor_code, datestart, da
         paste0(.,"&date_to=",End)
 
     })) %>%
+
     mutate(Data= lapply(URL, function(x){
       tryCatch({as_tibble(jsonlite::fromJSON(x))},error=function(e){NA})
     }))
 
 
-  fmt<- unnest(dat)
-  if(nrow(fmt)>0){
+  fmt <- unnest(dat, cols = c(Data))
+  if (nrow(fmt) > 0) {
 
-    fmt2<- fmt %>%
-      mutate(TimeStamp=as_datetime(DATE,tz = "Europe/Berlin")) %>%
-      rename(Value=VALUE) %>%
-      select(-c(URL,DATE)) %>%
-      select(TimeStamp, everything()) %>%
-      arrange(Sensor,TimeStamp)
+    fmt2 <-
+      fmt %>%
+      mutate(datetime = as_datetime(DATE, tz = "Europe/Berlin")) %>%
 
-    return(fmt2)
+      rename(st_id = SCODE) %>%
+      dplyr::rename_with(tolower) %>%
+
+      select(-c(url, date, start, end)) %>%
+      select(datetime, everything()) %>%
+      arrange(sensor, datetime) %>%
+
+      mutate(sensor = recode(sensor,
+                             'LT' = 'tair',
+                             'GS' = 'rad',
+                             'N' = 'preci',
+                             'SD' = 'radh'))
+
   } else {stop("None of the URLS is valid. Please modify the Input parameters")}
+
+  if (format == 'wide') {
+    return(tidyr::pivot_wider(fmt2, names_from = sensor, values_from = value))
+  } else if (format == 'long') {
+    return(fmt2)
+  } else { stop('Unrecognized format string. Use one of "wide" or "long".') }
 
 }
 
@@ -67,11 +86,12 @@ rg_get_province <- function(dburl=NULL, station_code, sensor_code, datestart, da
 #' It unifies both information returning the complete range of information present in the Open Data Portal South Tyrol.
 #' @param format string; digit "table" if the output should be a Dataframe or "spatial" for a spatial
 #' output as sf-object
+#' @source \link{https://github.com/mattia6690/MonalisR/blob/master/R/Processing.R}
 #' @importFrom jsonlite fromJSON
 #' @importFrom sf st_as_sf
 #' @export
 
-rg_province_info <- function(format="table"){
+rg_province_info <- function( format="table" ){
 
   stat      <- jsonlite::fromJSON("http://daten.buergernetz.bz.it/services/meteo/v1/stations")
   stat.prop <- stat$features$properties
