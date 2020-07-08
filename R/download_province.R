0263e57102f3c6f81356e53b06a35584122671c3
Simon Tscholl <tscholl.simon@gmail.com>
2020-07-08 14:47:52 +0200

    split download per year to avoid missing data in the beginning

--- R/download_province.R
+++ R/download_province.R
@@ -27,14 +27,35 @@ rg_province_get <- function(dburl=NULL, station_code, sensor_code, datestart, da
   #use url from province database if non given
   if(is.null(dburl)) dburl<- "http://daten.buergernetz.bz.it/services/meteo/v1/timeseries"
 
+  #If dateseq is more than one year sometimes data at the beginning is missing (because requested data is
+  #too large in memory?). Therefore the data is downloaded separately for each year.
+  #If the end date is 2019-12-31, for example, the server gives only the first value from this day at 00:00 am
+  #and then the download stops. Therefore one day is added to the end day to download all the values from the
+  #last day.
+  dateseq <- seq.Date(datestart, dateend, by = 'day')
+  date_df <-
+    dateseq %>%
+    split(year(.)) %>%
+    tibble::enframe(name = 'year', value = 'date') %>%
+    tidyr::unnest(date) %>%
+
+    dplyr::group_by(year) %>%
+    dplyr::summarise(Start = min(date),
+                     End = max(date), .groups = 'drop') %>%
+    dplyr::select(-year) %>%
+
+    mutate(End = End + 1) %>%
+    dplyr::mutate(dplyr::across(tidyselect::everything(), ~format(.x, format = "%Y%m%d")))
+
   dat <-
     expand.grid(station_code, sensor_code, stringsAsFactors = FALSE) %>%
-    as_tibble() %>%
+    tibble::as_tibble() %>%
     setNames(c("SCODE","Sensor")) %>%
 
-    mutate(Start = format(datestart, format="%Y%m%d")) %>%
-    mutate(End = format(dateend, format="%Y%m%d")) %>%
-    mutate(URL = pmap_chr(., function(SCODE, Sensor, Start, End){
+    dplyr::mutate(dates = list(date_df)) %>%
+    tidyr::unnest(dates) %>%
+
+    dplyr::mutate(URL = pmap_chr(., function(SCODE, Sensor, Start, End){
 
       dburl %>%
         paste0(.,"?station_code=",SCODE) %>%
@@ -45,40 +66,49 @@ rg_province_get <- function(dburl=NULL, station_code, sensor_code, datestart, da
 
     })) %>%
 
-    mutate(Data= lapply(URL, function(x){
+    dplyr::mutate(Data= lapply(URL, function(x){
       tryCatch({as_tibble(jsonlite::fromJSON(x))},error=function(e){NA})
     }))
 
+  #Some values are duplicated because we added on day to the end date for the download range. Therefore
+  #we remove those duplicated values by removing the first row (=last downloaded value) from each dataframe.
+  fmt <-
+    dat %>%
+    mutate(Data = map(Data, ~slice(., -1))) %>%
+    unnest(cols = c(Data))
 
-  fmt <- unnest(dat, cols = c(Data))
   if (nrow(fmt) > 0) {
 
     fmt2 <-
       fmt %>%
-      mutate(datetime = as_datetime(DATE, tz = "Europe/Berlin")) %>%
+      dplyr::mutate(datetime = as_datetime(DATE, tz = "Europe/Berlin")) %>%
 
-      rename(st_id = SCODE) %>%
+      dplyr::rename(st_id = SCODE) %>%
       dplyr::rename_with(tolower) %>%
 
-      select(-c(url, date, start, end)) %>%
-      select(st_id, sensor, datetime, everything()) %>%
-      arrange(st_id, sensor, datetime) %>%
+      dplyr::select(-c(url, date, start, end)) %>%
+      dplyr::select(st_id, sensor, datetime, everything()) %>%
+      dplyr::arrange(st_id, sensor, datetime) %>%
 
-      mutate(sensor = recode(sensor,
-                             'LT' = 'tair',
-                             'GS' = 'rad',
-                             'N' = 'preci',
-                             'SD' = 'radh')) %>%
+      dplyr::mutate(sensor = dplyr::recode(sensor,
+                                           'LT' = 'tair',
+                                           'GS' = 'rad',
+                                           'N' = 'preci',
+                                           'SD' = 'radh')) %>%
 
       #Sometimes duplicated records appear in table (often on 2018-10-28). Keep first row
-      distinct(st_id, sensor, datetime, .keep_all = T)
+      dplyr::distinct(st_id, sensor, datetime, .keep_all = T)
 
   } else {stop("None of the URLS is valid. Please modify the Input parameters")}
 
   if (format == 'wide') {
+
     return(tidyr::pivot_wider(fmt2, names_from = sensor, values_from = value))
+
   } else if (format == 'long') {
+
     return(fmt2)
+
   } else { stop('Unrecognized format string. Use one of "wide" or "long".') }
 
 }
