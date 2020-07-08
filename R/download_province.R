bdbe5c2aeebc794b2fd465620baf578da75f513e
Simon Tscholl <tscholl.simon@gmail.com>
2020-07-08 08:37:44 +0200

    edited description

--- R/download_province.R
+++ R/download_province.R
@@ -1,5 +1,6 @@
-#' @title Bulk download Meteorological Data (v2)
-#' @description Function for accessing the Meteorological Data. This function will replace the
+#' Bulk download Meteorological Data (v2)
+#'
+#' Function for accessing the Meteorological Data. This function will replace the
 #' Original Function in the next version. No save to CSV is possible anymore. On the other side
 #' it will be possible to speed up the actual processing times and to make multiple requests at once
 #' @param dburl URL; URL of the Province Database. If left empty the original API will be used
@@ -7,7 +8,6 @@
 #' @param sensor_code string; Abbreviation of the sensor of interest (e.g. "N" for Precipitation)
 #' @param datestart string; Starting time for the download in "Ymd" Format
 #' @param dateend string; End time for the download in "Ymd" Format
-#' @source \link{https://github.com/mattia6690/MonalisR}
 #' @importFrom dplyr mutate select rename arrange
 #' @importFrom magrittr "%>%" extract
 #' @importFrom lubridate as_datetime
@@ -60,13 +60,13 @@ rg_get_province <- function(dburl=NULL, station_code, sensor_code, datestart, da
 
 }
 
-#' @title Return Meteo South Tyrol Metainformation
-#' @description This function returns all the metainformation of the meteorological Data from
+#' Return Meteo South Tyrol Metainformation
+#'
+#' This function returns all the metainformation of the meteorological Data from
 #' the OpenData Portal South Tyrol, It is a combination of both former functions `getMeteoStat` and `getMeteoSensor`.
 #' It unifies both information returning the complete range of information present in the Open Data Portal South Tyrol.
 #' @param format string; digit "table" if the output should be a Dataframe or "spatial" for a spatial
 #' output as sf-object
-#' @source \link{https://github.com/mattia6690/MonalisR}
 #' @importFrom jsonlite fromJSON
 #' @importFrom sf st_as_sf
 #' @export
